385f37f3c0faf0d86ce277ac983c84e186bc4abb
Jun Shao <mr.shaojun@gmail.com>
2017-05-31 20:03:34 +1000

    Code clean up

--- src/web/app/signIn.component.ts
+++ src/web/app/signIn.component.ts
@@ -1,11 +1,7 @@
 import {
 	Component,
 	OnInit,
-	ApplicationRef
 } from '@angular/core';
-import {
-	SignInUpService
-} from "./signin.service";
 import {
 	CookieService
 } from 'ng2-cookies';
@@ -43,22 +39,17 @@ interface SignUp {
 export class SignInComponent implements OnInit {
 	ngOnInit(): void {}
 
-	constructor(private notificationService: NotificationService, private contextService: ContextService, private applicationRef: ApplicationRef, private apiService: ApiService, private router: Router) {}
+	constructor(private notificationService: NotificationService, private contextService: ContextService, private apiService: ApiService, private router: Router) {}
 
 	async onLogin() {
 		try {
 			const result = await this.apiService.raw.post("auth", this.model);
 			this.contextService.context.user = result.user;
 			this.contextService.context.role = this.model.role;
-			this.notificationService.info(result);
-			
 			this.router.navigateByUrl('/pdashboard');
-
-			// this.applicationRef.tick();
 		} catch (error) {
 			this.contextService.context.user = null;
 			this.contextService.context.role = null;
-			// this.applicationRef.tick();
 			this.notificationService.error(error);
 		}
 	}
